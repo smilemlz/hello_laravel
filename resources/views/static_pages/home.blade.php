2a7dec7a0e7a8c1392590a094b49927dd9ccf529
smilemlz <1499542428@qq.com>
2017-02-20 21:20:35 +0800

    finish layout and routes

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.default')
 @section('title','主页')
 @section('content')
-<h1>主页</h1>
+<div class="jumbotron">
+	<h1>laravel</h1>
+	<p class="lead">
+		您现在所看到的是<a href="https://laravel-china.org/laravel-tutorial/5.1">laravel 入门教程</a>de 
+	</p>
+	<p>
+		一切，从这里开始
+	</p>
+	<p  >
+		<a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a>
+	</p>
+</div>
 @stop
